21b2928288f3a3b7c4c83ba17626baaaa49f9840
Maxim Zzzz <z_connect@mail.ru>
2016-06-01 00:31:08 +0300

    css

--- public/admin.php
+++ public/admin.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Moskini
- * Date: 31.05.2016
- * Time: 16:55
- */ 
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="header">
+        <h1>Widget Corp</h1>
+    </div>
+    <div id="main">
+        <div id="navigator">
+            &nbsp;
+        </div> <!-- navigator -->
+        <div id="page">
+            <h2>Admin Menu</h2>
+            <p>Welcome to the admin area.</p>
+            <ul>
+                <li><a href="manage_content.php">Manage Website Content</a></li>
+                <li><a href="manage_admins.php">Manage Admin User</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div> <!-- page -->
+    </div> <!--main -->
+    <div id="footer">
+        Copyright 20xx, Widget corp
+    </div> <!-- footer -->
+
+</body>
+</html>
