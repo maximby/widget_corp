9d409e43fdce012823aa420b9c11fc92fe5c2167
Maxim Zzzz <z_connect@mail.ru>
2016-06-01 14:47:50 +0300

    рефакторинг выбора страниц

--- includes/functions.php
+++ includes/functions.php
@@ -13,7 +13,7 @@ function find_all_subjects()
     global $connection;
     $query = 'SELECT * ';
     $query .= 'FROM subjects ';
-    $query .= 'WHERE visible = 1 ';
+   // $query .= 'WHERE visible = 1 ';
     $query .= 'ORDER BY position ASC';
     $subject_set = mysqli_query($connection, $query);
     confirm_query($subject_set);
@@ -24,26 +24,82 @@ function find_all_subjects()
 function find_pages_for_subjects($subject_id)
 {
     global $connection;
+    $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
     $query = 'SELECT * ';
     $query .= 'FROM pages ';
     $query .= 'WHERE visible = 1 ';
-    $query .= 'AND subject_id = ' . $subject_id;
+    $query .= 'AND subject_id = ' . $safe_subject_id;
     $query .= ' ORDER BY position ASC';
     $page_set = mysqli_query($connection, $query);
     confirm_query($page_set);
     return $page_set;
 }
 
-// Навигация принимает 2 аргумента
-// текущий выбранный id subject
-// и id текущей страницы
-function navigation($subject_id, $page_id)
+// Найти объект по id
+function find_subject_by_id($subject_id) {
+    global $connection;
+
+    $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+    $query = 'SELECT * ';
+    $query .= 'FROM subjects ';
+    $query .= 'WHERE  id = ' . $safe_subject_id;
+    $query .= ' LIMIT 1';
+    $subject_set = mysqli_query($connection, $query);
+    confirm_query($subject_set);
+    if ($subject = mysqli_fetch_assoc($subject_set)) {
+        return $subject;
+    } else {
+        null;
+    }
+}
+
+// Найти страницу по id
+function find_page_by_id($page_id) {
+    global $connection;
+
+    $safe_page_id = mysqli_real_escape_string($connection, $page_id);
+    $query = 'SELECT * ';
+    $query .= 'FROM pages ';
+    $query .= 'WHERE  id = ' . $safe_page_id;
+    $query .= ' LIMIT 1';
+    $page_set = mysqli_query($connection, $query);
+    confirm_query($page_set);
+    if ($page = mysqli_fetch_assoc($page_set)) {
+        return $page;
+    } else {
+        null;
+    }
+
+}
+
+function find_selected_page() {
+    global $current_subject;
+    global $current_page;
+
+    if (isset($_GET['subject'])) {
+        $current_subject = find_subject_by_id($_GET['subject']);
+        $current_page = null;
+    } elseif (isset($_GET['page'])) {
+        $current_page = find_page_by_id($_GET['page']);
+        $current_subject = null;
+    } else {
+        $current_subject = null;
+        $current_page = null;
+    }
+
+}
+
+// Навигация принимает 2 аргумент
+// текущий выбранный  subject array or null
+// и текущей страницы array or null
+function navigation($subject_array, $page_array)
 {
     $output = "<ul class=\"subjects\">";
     $subject_set = find_all_subjects();
     while ($subject = mysqli_fetch_assoc($subject_set)):
         $output .= "<li ";
-        if ($subject['id'] == $subject_id) {
+        if ($subject_array && $subject['id'] == $subject_array['id']) {
             $output .= "class=\"selected\"";
         }
         $output .= " >";
@@ -56,7 +112,7 @@ function navigation($subject_id, $page_id)
         $pages_set = find_pages_for_subjects($subject['id']);
         while ($page = mysqli_fetch_assoc($pages_set)):
             $output .= "<li ";
-            if ($page['id'] == $page_id) {
+            if ($page_array && $page['id'] == $page_array['id']) {
                 $output .= "class=\"selected\"";
             };
             $output .= "\" >";
@@ -76,3 +132,5 @@ function navigation($subject_id, $page_id)
     $output .= "</ul>";
     return $output;
 }
+
+
--- public/manage_content.php
+++ public/manage_content.php
@@ -1,31 +1,33 @@
 <?php
-ini_set('display_errors','On');
-error_reporting('E_ALL');
+
 
 require_once __DIR__ . '/../includes/functions.php';
 require_once __DIR__ . '/../includes/db_connection.php';
 include __DIR__ . '/../includes/layouts/header.php';
 
-if (isset($_GET['subject'])) {
-    $selected_subject_id = $_GET['subject'];
-    $selected_page_id = null;
-} elseif (isset($_GET['page'])) {
-    $selected_page_id = $_GET['page'];
-    $selected_subject_id = null;
-} else {
-    $selected_subject_id = null;
-    $selected_page_id = null;
-}
-?>
+find_selected_page();
+ ?>
 
     <div id="main">
         <div id="navigator">
-            <?php echo navigation($selected_subject_id, $selected_page_id) ?>
+            <?php echo navigation($current_subject, $current_page) ?>
         </div> <!-- navigator -->
         <div id="page">
-            <h2>Manage Content</h2>
-            <?php echo $selected_subject_id;?> <br />
-            <?php echo $selected_page_id;?>
+
+            <?php
+            if ($current_subject) {?>
+                <h2>Manage Subject</h2>
+             <?php
+                echo "Menu name: " . $current_subject['menu_name'];
+            } elseif ($current_page) {?>
+                <h2>Manage Page</h2>
+                <?php
+
+                echo "Menu name: " . $current_page['menu_name'];
+            } else {
+                 echo "Выберите объект или страницу";
+            }
+            ?>
 
         </div> <!-- page -->
     </div> <!--main -->
