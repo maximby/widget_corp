a8e39dad32f6cd8cae24ac69fe41deec68969d9e
Maxim Zzzz <z_connect@mail.ru>
2016-06-01 11:23:51 +0300

    рефакторинг

--- includes/functions.php
+++ includes/functions.php
@@ -5,3 +5,28 @@ function confirm_query($result_set) {
         die("Запрос к БД невыполнен");
     }
 }
+
+// Поиск всех объектов
+function find_all_subjects() {
+    global $connection;
+    $query = 'SELECT * ';
+    $query .= 'FROM subjects ';
+    $query .= 'WHERE visible = 1 ';
+    $query .= 'ORDER BY position ASC';
+    $subject_set = mysqli_query($connection, $query);
+    confirm_query($subject_set);
+    return $subject_set;
+}
+
+// Поиск всех страниц для объекта
+function find_pages_for_subjects($subject_id) {
+    global $connection;
+    $query = 'SELECT * ';
+    $query .= 'FROM pages ';
+    $query .= 'WHERE visible = 1 ';
+    $query .= 'AND subject_id = ' . $subject_id;
+    $query .= ' ORDER BY position ASC';
+    $page_set = mysqli_query($connection, $query);
+    confirm_query($page_set);
+    return $page_set;
+}
--- public/manage_content.php
+++ public/manage_content.php
@@ -2,30 +2,16 @@
 require_once __DIR__ . '/../includes/functions.php';
 require_once __DIR__ . '/../includes/db_connection.php';
 
-$query = 'SELECT * ';
-$query .= 'FROM subjects ';
-$query .= 'WHERE visible = 1 ';
-$query .= 'ORDER BY position ASC';
-$subject_set = mysqli_query($connection, $query);
-confirm_query($subject_set);
-
 include __DIR__ . '/../includes/layouts/header.php' ?>
     <div id="main">
         <div id="navigator">
            <ul class="subjects">
            <?php
+           $subject_set = find_all_subjects();
              while ($subject = mysqli_fetch_assoc($subject_set)):
            ?>
                <li><?php echo $subject['menu_name'] ;
-
-                       $query = 'SELECT * ';
-                       $query .= 'FROM pages ';
-                       $query .= 'WHERE visible = 1 ';
-                       $query .= 'AND subject_id = ' . $subject['id'];
-                       $query .= ' ORDER BY position ASC';
-                       $pages_set = mysqli_query($connection, $query);
-                       confirm_query($pages_set);
-
+                       $pages_set = find_pages_for_subjects($subject['id'])
                    ?>
                    <ul class="pages">
                        <?php while ($pages = mysqli_fetch_assoc($pages_set)):?>
